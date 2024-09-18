58bacaa37cdf6f68416e2c5b6b07b89eac0899a1
Sankalp Shankar <sankalps.23.beai@acharya.ac.in>
2024-09-18 11:33:40 +0530

    18-sep

---
+++ list and matrices.R
@@ -0,0 +1,21 @@
+# Create a list for each employee
+employee_nAME <- c("Employee 1", "Employee 2", "Employee 3", "Employee 4")
+empid <- c(102, 103, 104, 105)
+
+
+
+# Print the employee list
+print(employee_list)
+
+num_of_emplaoyes <- 4
+employee_list <- list(employee_nAME, empid, num_of_emplaoyes)
+employee_list
+
+uid <- c(1, 2, 3, 4)
+uni_name <- c("Acharya", "C.M.R", "Cambridge", "SIT")
+strength <- c(1000, 2000, 3000, 4000)
+result_percent <- c(80, 85, 90, 95)
+location <- c("Soladevanahalli", "Marathalli", "K.R. Puram", "Mysuru")
+
+uni_data <- data.frame(uid, uni_name, strength, result_percent, location)
+uni_data
